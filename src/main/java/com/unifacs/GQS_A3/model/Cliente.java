8147d5067578554595de0f5b919733bb0481b50b
luanc <luancerqueira.s2@gmail.com>
2025-09-18 20:53:28 -0300

    feat(cliente): Tabela e rotas adicionadas para a entidade Cliente

---
+++ src/main/java/com/unifacs/GQS_A3/model/Cliente.java
@@ -0,0 +1,60 @@
+package com.unifacs.GQS_A3.model;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name ="cliente")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String senha;
+    private String email;
+    private LocalDate dataNascimento;
+
+    public Cliente(){}
+
+    public Cliente(String nome, String senha, String email, LocalDate dataNascimento){
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setDataNascimento(LocalDate dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+    public LocalDate getDataNascimento(){
+        return dataNascimento;
+    }
+}
+
