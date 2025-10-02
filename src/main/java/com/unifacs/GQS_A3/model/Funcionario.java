eaab88b7faa506e8eb7e202ca1a3d643ca63b150
luan <luancerqueira.s2@gmail.com>
2025-10-02 17:11:40 -0300

    feat: Adicionada feat de modificar funcionario

--- src/main/java/com/unifacs/GQS_A3/model/Funcionario.java
+++ src/main/java/com/unifacs/GQS_A3/model/Funcionario.java
@@ -84,4 +84,16 @@ public class Funcionario {
     public void setDataNascimento(LocalDate dataNascimento) {
         this.dataNascimento = dataNascimento;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
