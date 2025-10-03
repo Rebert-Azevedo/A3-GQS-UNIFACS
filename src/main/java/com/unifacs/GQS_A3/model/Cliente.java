989480a84c5ae16f83602d5da54539a589843b94
luan <luancerqueira.s2@gmail.com>
2025-10-02 21:18:59 -0300

    Refactor: Utilizando Lombok anottations para os Getters, Setters e Contrutores

--- src/main/java/com/unifacs/GQS_A3/model/Cliente.java
+++ src/main/java/com/unifacs/GQS_A3/model/Cliente.java
@@ -1,10 +1,18 @@
 package com.unifacs.GQS_A3.model;
 
 import java.time.LocalDate;
-import java.util.Date;
-
+import java.time.LocalDateTime;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name ="cliente")
 public class Cliente {
@@ -13,50 +21,18 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String senha;
+
     private LocalDate dataNascimento;
 
-    public Cliente(){}
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
 
-    public Cliente(String nome, String senha, String email, LocalDate dataNascimento){
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-        this.dataNascimento = dataNascimento;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento){
-        this.dataNascimento = dataNascimento;
-    }
-
-    public LocalDate getDataNascimento(){
-        return dataNascimento;
-    }
 }
 
