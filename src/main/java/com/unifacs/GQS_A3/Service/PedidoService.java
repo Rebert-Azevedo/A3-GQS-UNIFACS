ece3a23076481eb437917beb6083f289d77b2629
JonatasGomes <jonatasgomeslima21@gmail.com>
2025-10-16 19:41:34 -0300

    feat: cria serviço PedidoService para lógica de criação de pedidos

--- src/main/java/com/unifacs/GQS_A3/Service/PedidoService.java
+++ src/main/java/com/unifacs/GQS_A3/Service/PedidoService.java
@@ -1,4 +1,31 @@
 package com.unifacs.GQS_A3.Service;
 
+import com.unifacs.GQS_A3.model.Pedido;
+import com.unifacs.GQS_A3.model.Produto;
+import com.unifacs.GQS_A3.Repository.PedidoRepository;
+import com.unifacs.GQS_A3.Repository.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
 public class PedidoService {
+    private final PedidoRepository pedidoRepository;
+    private final ProdutoRepository produtoRepository;
+
+    @Autowired
+    public PedidoService(PedidoRepository pedidoRepository, ProdutoRepository produtoRepository) {
+        this.pedidoRepository = pedidoRepository;
+        this.produtoRepository = produtoRepository;
+    }
+
+    @Transactional
+    public Pedido criarPedido(String nomeCliente) {
+        Pedido novoPedido = new Pedido();
+        novoPedido.setCliente(nomeCliente);
+        return pedidoRepository.save(novoPedido);
+    }
 }
